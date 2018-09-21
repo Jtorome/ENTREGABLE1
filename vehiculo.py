79fdab9e2bb802fafc797da523feb94de3dcd2a4
Jtorome <39070265+Jtorome@users.noreply.github.com>
2018-09-20 20:48:34 -0500

    Add files via upload

---
+++ vehiculo.py
@@ -0,0 +1,53 @@
+class VEHICULO:
+
+    ListaVehiculos=[]
+
+    def __init__(self, Placa, Color, TipoVehiculo, CantidadAsientos, Conductor):
+
+        '''ATRIBUTOS
+        self._Placa
+        self._Color
+        self._TipoVehiculos
+        self._CantidadAsientos
+        self._Conductor
+        '''
+
+        self.setPlaca(placa)
+        self.setColor(color)
+        self.setTipoVehiculos(tipoVehiculos)
+        self.setCantidadAsientos(cantidadAsientos)
+        self.setConductor(conductor)
+        VEHICULO.ListaVehiculos.append(self)
+
+        def setPlaca(self, placa):
+            self._Placa=placa
+
+        def getPlaca(self):
+            return self._Placa
+
+        def setColor(self, color):
+            self._Color=color
+
+        def getColor(self):
+            return self._Color
+
+        def SetTipoVehiculo(self, tipoVehiculo):
+            self._TipoVehiculo=TipoVehiculo
+
+        def getTipoVehiculo(self):
+            return self._TipoVehiculo
+
+        def SetCantidadAsientos(self, cantidadAsientos):
+            self._CantidadAsientos=int(CantidadAsientos)
+
+        def getCantidadAsientos(self):
+            return self._CantidadAsientos
+
+        @staticmethod
+        def setConductor(self, conductor):
+            self._Conductor=conductor
+            conductor.setVehiculo(self)
+
+        @staticmethod
+        def getConductor(self):
+            return self._conductor
