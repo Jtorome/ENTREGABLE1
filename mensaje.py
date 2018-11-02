41de56a9b553345d28fd37d2490a9e56041f726e
Sergio Esteban Cabrera Eraso <43325149+Scabrerax@users.noreply.github.com>
2018-11-02 13:49:03 -0500

    Add files via upload

--- mensaje.py
+++ mensaje.py
@@ -31,9 +31,8 @@ class MENSAJE:
 			
 	"MenuPasajero" :"""
 					Inicio sesion como pasajero
-			1. Ver servicios disponibles.
-			2. Informacion sobre mi viaje.
-			3. Cerrar sesion.
+			1. Ver servicios disponibles
+			2. Cerrar sesion
 			""",
 
 	"MenuConductor" :"""
@@ -66,9 +65,8 @@ class MENSAJE:
 
 	"MenuFormatoViajeActual":"""
 	1. Cambiar informacion.
-	2. Eliminar servicio.
-	3. Atras.""",
-
+	2. Atras.""",
+			
 	"Opcion":"Ingrese la opcion: ",
 	"NoViaje":"No tiene ningun viaje programado.",
 	"NoHistorial":"No ha echo ningun viaje Intercampus.",
@@ -109,15 +107,8 @@ class MENSAJE:
 	Ingrese el servicio que desea tomar: """,
 	"FormatoSetActivo": "{0}, {1}.",
 	"FormatoViajeActual": "Hora de encuentro: {0}, sitio de encuentro: {1}, lugar de inicio: {2}, lugar de llegada: {3}, asientos disponibles: {4}",
-	"SinServiciosCon": "Usted no tiene viaje programado.",
-	"NoPuedeTomarSer": "Usted no puede tomar este servicio ya que usted mismo lo propuso.",
-	"ViajeLleno": "Ya no quedan cupos en este viaje por favor toma otro.",
-	"RegistradoEnSer": "Usted a quedado registrado en el servicio que parte a las {0} del {1} hacia {2}",
-	"AsientosMaximos": "La cantidad maxima de asientos disponibles para un {0} son {1}",
-	"HoraYaPaso": "La hora ingresada ya paso. Por favor ingrese una hora que no haya pasado.",
-	"FormatoInfoSer": "Informacion sobre el servicio.\nHora de encuentro: {0}, sitio de encuentro: {1}, lugar de inicio: {2}, lugar de llegada: {3}, asientos disponibles: {4}, Fecha: {5}.\n",
-	"FormatoInfoCon": "Informacion Sobre el conductor.\nNombre: {0}, Celular: {1}, servicios hechos: {2}, Acumulado de calificacion: {3}.\n",
-	"FormatoInfoVeh": "Informacion sobre el vehiculo.\nPlaca: {0}, color: {1}, tipo de vehiculo: {2}.\n"
+		"IngresarFecha":"Favor ingresar fecha en formato yymmdd(maximo un mes de antelacion)",
+	"FechaIncorrecta":"Formato invalido(yymmdd) o fecha limite excedida."
 	}
 	
 
@@ -171,26 +162,47 @@ class MENSAJE:
 			4. Log out 
 			""",
 			
-	"Opcion": "Select an option:",   
-	"NoViaje":"You have no scheduled trip ",
-	"NoHistorial":"You have not done any Intercampus trip ", 
-	"InfoVehiculo":"Enter the vehicle information to use ",
+	"Opcion":"Select an option: ",
+	"NoViaje":"You have not sheduled trip.",
+	"NoHistorial":"You have not done any intercampus trip.",
+	"InfoVehiculo":"""	Enter the vehicle information to use.
+	Select 3 to cancel""",
 	"IngresarPlaca":"Enter the number of license plate: ",
+	"PlacaInvalida": "License plate already existing in the system.",
 	"IngresarColor":"Enter the color of vehicle: ",
-	"IngresarTipoVehiculo":"Enter the vehicle type: ",
-	"IngresarCantidadAsientos":"Ingrese la cantidad total de asientos del vehiculo: ",
-	"OpcionIncorrecta": "{0} is not a valid choice.",
+	"IngresarTipoVehiculo":"Enter the vehicle type(car or motorcycle): ",
+	"IngresarCantidadAsientos": "Enter the total number of seats of the vehicle: ",
+	"ActivarVehiculo":"Will you use this vehicle on your next trip? (Yes or No)",
+	"OpcionIncorrecta":"{0} Is not valid choice. ",
 	"IngresarCorreo":"Enter your mail: ",
-	"IngresarContrasena": "Enter your password: ",
-	"IngresarNombre":"Enter your name: ",
-	"IngresarCell": "Enter your cellphone: ",
-	"Antes@": "Plase enter your user before that @",
+	"IngresarContrasena": "Enter your password:  ",
+	"IngresarNombre": "Enter your name:",
+	"IngresarCell": "Enter your cellphone",
+	"Antes@": "Please write your user before that @",
 	"Salir": "Bye Bye",
-	"IngresarDescripcion": "How was your travel ?: ",
-	"CorreoInvalido": "The email address you have entered is already registered: ",
-	"CorreoInexistente": "The email you entered is not registered: ",
-	"ContrasenaInvalida": "Invalid password.",
+	"IngresarDescripcion": "Please enter the description: ",
+	"CorreoInvalido": "Existing mail please enter another: ",
+	"CorreoInexistente": "The mail is not registered, please register or enter a valid mail",
+	"ContraOCorreoInvalido": "Mail or password invalid.",
 	"MensajeInicioSesion": """        Login
-		Press 3 to exit."""
+		Enter 3 to exit.""",
+	"CerradoSesion": "Make closed session. ",
+	"SinServicios": "There are no services available",
+	"FormatoVerServicios": "{0}, Meeting time: {1}, Meetin place: {2}, Start place: {3}, Place of arrival: {4}, Available seats: {5}, Name of condutor: {6}.\n",
+	"InfoServicio": """	Enter the information of service.
+	Enter 3 to cancel.""",
+	"IngresarHoraEncuentro": "Enter the meeting time(24h): ",
+	"IngresarSitioEncuentro": "Enter the meeting place(Volador, minas, rio): ",
+	"IngresarLugarInicio": "Enter the place where star the service(Example:bloque M8, 12, 4): ",
+	"IngresarLugarFin": "Enter the place where the service will end(Example:bloque M8, 12, 4): ",
+	"IngresarAsientosDisponibles": "Enter the number of seats available: ",
+	"ServicioEnCurso": "You already have an ongoing service, please finish that before you schedule another.",
+	"IngreseServicioEscogido": """
+	Enter 0 to exit.
+	Enter the servic that wish take: """,
+	"FormatoSetActivo": "{0}, {1}.",
+	"FormatoViajeActual": "Meeting time: {0}, Meeting place: {1}, Start place: {2}, Place of arrival: {3}, Available seats: {4}",
+	"IngresarFecha":"Please enter date in format yymmdd(maximum one month in advance)",
+	"FechaIncorrecta":"Invalid format(yymmdd) or limit date exceeded."
 
 			}
