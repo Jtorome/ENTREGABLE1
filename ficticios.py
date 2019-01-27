f7aa57e4f653dbf7efdb6d2504550aa474a3dca3
Jtorome <39070265+Jtorome@users.noreply.github.com>
2019-01-27 11:17:12 -0500

    .

--- ficticios.py
+++
@@ -1,9 +0,0 @@
-class FICTICIOS:
-
-	nombres=["Juan", "Camilo", "Maria", "Julia", "Manuela","Eduardo", "Sergio", "Cristian", "Jhonatan", "Louis", "Slappy", "Rose", "Yami"]
-	apellidos=["Toro", "Ramirez", "Cabrera", "Santos", "Cordoba", "Vermillion", "Chronos", "Mejia", "Sanchez", "Montoya", "Cortes", "Heisenberg"]
-	
-
-	@staticmethod
-	def DatosFicticios(numero):
-		for i in range(numero):
