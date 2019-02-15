dba4c37bf88877b35176c5c5a7acf8a3e134b99e
Jtorome <39070265+Jtorome@users.noreply.github.com>
2019-02-15 11:32:43 -0500

    .

--- correr.py
+++ correr.py
@@ -298,3 +298,76 @@ class CORRER:
     #con=conductor1.getVehiculos()
     #for vehi in con:
 #       print(vehi.getTipoVehiculo())
+
+"""@staticmethod
+    def DatosFicticiosTxt():
+        archivo=open("registro.txt", "r").readlines()
+        for line in archivo:
+            line=line.split(',')
+            if "PASAJERO" == line[0]:
+                if PASAJERO.BuscadorDePasajeros(line[1]) == None:
+                    PASAJERO(line[1], line[2], line[3], line[4], float(line[5].split()[0]))
+            elif "CONDUCTOR" == line[0]:
+                if CONDUCTOR.BuscadorDeConductor(line[1]) == None:
+                    CONDUCTOR(line[1], line[2], line[3], line[4], line[5], line[6].split()[0])
+            elif "VEHICULO" == line[0]:
+                if  VEHICULO.VerificarPlaca(line[1]) == False:
+                    for conductor in CONDUCTOR.ListaConductores:
+                        correo=line[6].split()
+                        if correo[0] == conductor.getCorreo():
+                            VEHICULO(line[1], line[2], line[3], line[4], line[5], conductor, line[7].split()[0])
+            elif "SERVICIO" == line[0]:
+                if SERVICIO.BuscadorDeServicio(line[1], line[6], line[7]) == None:
+                    for conductor in CONDUCTOR.ListaConductores:
+                        correo=line[6].split()
+                        if correo[0] == conductor.getCorreo():
+                            vehiculo=VEHICULO.BuscadorDeVehiculo(line[7])
+                            SERVICIO(line[1], line[2], line[3], line[4], line[5], conductor, vehiculo, line[8], line[9].split()[0])
+
+        for servicio in SERVICIO.ListaServicios:
+            text=servicio.getInformacionSerCompleta().split(',')
+            for line in archivo:
+                line=line.split(',')
+                if line[0]=="SERVICIO":
+                    if len(line)==11 and text[1]==line[1] and text[6]==line[6] and text[8]==line[8]:
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[10].split()[0])
+                        servicio.setPasajeros(pasajero)
+                    elif len(line)==12 and text[1]==line[1] and text[6]==line[6] and text[8]==line[8]:
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[10])
+                        servicio.setPasajeros(pasajero)
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[11].split()[0])
+                        servicio.setPasajeros(pasajero)
+                    elif len(line)==13 and text[1]==line[1] and text[6]==line[6] and text[8]==line[8]:
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[10])
+                        servicio.setPasajeros(pasajero)
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[11])
+                        servicio.setPasajeros(pasajero)
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[12].split()[0])
+                        servicio.setPasajeros(pasajero)
+                    elif len(line)==14 and text[1]==line[1] and text[6]==line[6] and text[8]==line[8]:
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[10])
+                        servicio.setPasajeros(pasajero)
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[11])
+                        servicio.setPasajeros(pasajero)
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[12])
+                        servicio.setPasajeros(pasajero)
+                        pasajero=PASAJERO.BuscadorDePasajeros(line[13].split()[0])
+                        servicio.setPasajeros(pasajero)
+                    SERVICIO.ActualizarSerDis()
+
+        for line in archivo:
+            line=line.split(',')
+            if "CALIFICACION"==line[0]:
+                if len(line)==8:
+                    servicio=SERVICIO.BuscadorDeServicio(line[4], line[5], line[6].split()[0])
+                    pasajero=PASAJERO.BuscadorDePasajeros(line[7].split()[0])
+                    CALIFICACION(line[1], line[2], line[3], servicio)
+                    pasajero.getServicioNoCalificado().remove(servicio)
+                elif len(line)==5:
+                    pasajero=PASAJERO.BuscadorDePasajeros(line[3])
+                    conductor=CONDUCTOR.BuscadorDeConductor(line[4].split()[0])
+                    CALIFICACION(line[1], line[2], pasajero)
+                    conductor.getPasajeroNoCalificado().remove(pasajero)
+            elif "COMENTARIO"==line[0]:
+                persona=PERSONA.BuscarPersona(line[2])
+                COMENTARIO(line[1], persona, line[3].split()[0])"""
